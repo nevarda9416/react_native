84d7650c571848ad545a1169a05743ac2a1b1895
TuDT <tudt@ecomobi.com>
2023-03-15 13:04:57 +0700

    - [React Native]: Props in component

--- niit_iproduct/HelloWorldApp.tsx
+++ niit_iproduct/HelloWorldApp.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-
+import {StyleSheet, Text, View} from 'react-native';
+type GreetingProps = {
+  name: string;
+};
+const styles = StyleSheet.create({
+  center: {
+    color: '#4630EB'
+  }
+});
+const Greeting = (props: GreetingProps) => {
+  return (
+    <View>
+      <Text style={styles.center}>Hello {props.name}!</Text>
+    </View>
+  )
+};
 const HelloWorldApp = () => {
   return (
     <View
@@ -9,6 +23,9 @@ const HelloWorldApp = () => {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
+        <Greeting name='Rexxar'/>
+        <Greeting name='Jaina'/>
+        <Greeting name='Valeera'/>
       <Text style={{color:'#F00F00'}}>Hello Tú!</Text>
     </View>
   );
